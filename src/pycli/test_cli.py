eccd843e88a325a440ad18949cafc34b437c9488
DevOps Jeremy <DevOpsJeremy@gmail.com>
2025-12-23 02:33:55 -0800

    update with version

--- src/pycli/test_cli.py
+++ src/pycli/test_cli.py
@@ -5,5 +5,6 @@ from .cli import hello
 def test_hello():
     runner = CliRunner()
     result = runner.invoke(hello)
+
     assert result.exit_code == 0
     assert result.output == "Hello world\n"
